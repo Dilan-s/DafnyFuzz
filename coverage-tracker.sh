560ba92d3e6601e14e419c83e7bef0f7d6c6df66
Dilan Sheth <ds1619@cloud-vm-41-57.doc.ic.ac.uk>
2023-04-24 19:44:20 +0100

    update coverage information

--- coverage-tracker.sh
+++ coverage-tracker.sh
@@ -15,7 +15,7 @@ dafny_binary=$(pwd)
 cd "$directory"
 
 x=0
-while [ $x -le 0 ]; do
+while [ true ]; do
   cd "$directory"
 
   echo "Test number $x"
@@ -26,32 +26,42 @@ while [ $x -le 0 ]; do
     x=$(( $x + 1 ))
     continue;
   fi
-#  cp test.dfy tests/"test$x.dfy"
+  cp test.dfy tests/"test$x.dfy"
   # css
   #./src/main/dafny_compiler/dafny/Binaries/Dafny /noVerify /compileTarget:cs /spillTargetCode:3 test.dfy
-
+  
   for file in tests/*.dfy
   do
     echo "Attempting to run $file"
 
     # GO
     cd "$dafny_binary"
-    echo "coverlet . --target \"dotnet\" --targetargs \"Dafny.dll /noVerify /compile:2 /compileTarget:go ../../../../../"$file" /out:/tmp/dv\" -f json --merge-with \"../../../../../coverage/\"" | bash
+    echo "GO File"
+    echo "coverlet . --target \"dotnet\" --targetargs \"Dafny.dll /noVerify /compile:2 /compileTarget:go ../../../../../"$file" /out:/tmp/dv\" -f json --merge-with \"coverage.json\"" | bash
 
     # js
     cd "$dafny_binary"
-    echo "coverlet . --target \"dotnet\" --targetargs \"Dafny.dll /noVerify /compile:2 /compileTarget:js ../../../../../"$file" /out:/tmp/dv\" -f json --merge-with \"../../../../../coverage/\"" | bash
+    echo "JS File"
+    echo "coverlet . --target \"dotnet\" --targetargs \"Dafny.dll /noVerify /compile:2 /compileTarget:js ../../../../../"$file" /out:/tmp/dv\" -f json --merge-with \"coverage.json\"" | bash
+
 
     # java
     cd "$dafny_binary"
-    echo "coverlet . --target \"dotnet\" --targetargs \"Dafny.dll /noVerify /compile:2 /compileTarget:java ../../../../../"$file" /out:/tmp/dv\" -f json --merge-with \"../../../../../coverage/\"" | bash
+    echo "JAVA File"
+    echo "coverlet . --target \"dotnet\" --targetargs \"Dafny.dll /noVerify /compile:2 /compileTarget:java ../../../../../"$file" /out:/tmp/dv\" -f json --merge-with \"coverage.json\"" | bash
+
 
     # py
     cd "$dafny_binary"
-    echo "coverlet . --target \"dotnet\" --targetargs \"Dafny.dll /noVerify /compile:2 /compileTarget:py ../../../../../"$file" /out:/tmp/dv\" -f json --merge-with \"../../../../../coverage/\"" | bash
+    echo "PY File"
+    echo "coverlet . --target \"dotnet\" --targetargs \"Dafny.dll /noVerify /compile:2 /compileTarget:py ../../../../../"$file" /out:/tmp/dv\" -f json --merge-with \"coverage.json\"" | bash
+
 
     cd "$directory"
   done
+
+  cd "$dafny_binary"
+  cp coverage.json "$directory"
 
   rm -rf tests/* || true
   x=$(( $x + 1 ))
