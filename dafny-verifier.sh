9c5824e3e99a2413d14f9b4ec28999474084f926
Sheth, Dilan <dilan.sheth19@imperial.ac.uk>
2023-05-14 09:18:07 +0100

    Do not look through directory if empty

--- dafny-verifier.sh
+++ dafny-verifier.sh
@@ -53,118 +53,124 @@ while [ true ]; do
   #./src/main/dafny_compiler/dafny/Binaries/Dafny /noVerify /compileTarget:cs /spillTargetCode:3 test.dfy
   cd "$directory"
   y=0
-  for file in tests-minimized/*
-  do
-    echo "Expecting validation to succeed for $file"
+  if [ "$(ls -A tests-minimized/)" ];
+  then
+    for file in tests-minimized/*.dfy
+    do
+      echo "Expecting validation to succeed for $file"
 
-    timeout -s SIGKILL $t ./src/main/dafny_compiler/dafny/Binaries/Dafny verify $file > tmp.txt 2>&1
-    if [ $? -eq 4 ]
-    then
-      echo "Verification error found in test $x - correct validation of the file $file"
-      mkdir "errors/verificationErrors/$x" || true
-      mkdir "errors/verificationErrors/$x/correct" || true
-      cp $file "errors/verificationErrors/$x/correct/test-$y.dfy"
-      cat tmp.txt > "errors/verificationErrors/$x/correct/verificationOutput-$y.txt"
-    fi
-    rm -rf test.dfy || true
-    rm -rf tmp.txt || true
-    y=$(( $y + 1))
-  done
-
-  cd "$directory"
-  y=0
-  for file in tests-incorrect/*
-  do
-    echo "Expecting validation to fail for $file"
-    cp "$file" test.dfy
-
-    timeout -s SIGKILL $t ./src/main/dafny_compiler/dafny/Binaries/Dafny verify test.dfy > tmp.txt 2>&1
-    code=$?
-    if [[ $code -ne 4 && $code -lt 5 ]]
-    then
-      echo "Verification error found in test $x - incorrect validation of file $file"
-      mkdir "errors/verificationErrors/$x" || true
-      mkdir "errors/verificationErrors/$x/incorrect" || true
-      cp $file "errors/verificationErrors/$x/incorrect/test-$y.dfy"
-      cat tmp.txt > "errors/verificationErrors/$x/incorrect/verificationOutput-$y.txt"
-    fi
-    rm -rf test.dfy || true
-    rm -rf tmp.txt || true
-    y=$(( $y + 1))
-  done
+      timeout -s SIGKILL $t ./src/main/dafny_compiler/dafny/Binaries/Dafny verify $file > tmp.txt 2>&1
+      if [ $? -eq 4 ]
+      then
+        echo "Verification error found in test $x - correct validation of the file $file"
+        mkdir "errors/verificationErrors/$x" || true
+        mkdir "errors/verificationErrors/$x/correct" || true
+        cp $file "errors/verificationErrors/$x/correct/test-$y.dfy"
+        cat tmp.txt > "errors/verificationErrors/$x/correct/verificationOutput-$y.txt"
+      fi
+      rm -rf tmp.txt || true
+      y=$(( $y + 1))
+    done
+  fi
 
   cd "$directory"
   y=0
-  for file in tests/*.dfy
-  do
-    echo "Attempting to run $file"
-    cp "$file" test.dfy
+  if [ "$(ls -A tests-incorrect/)" ];
+  then
+    for file in tests-incorrect/*.dfy
+    do
+      echo "Expecting validation to fail for $file"
 
-    # GO
-    cd "$directory"
-    timeout -s SIGKILL $t ./src/main/dafny_compiler/dafny/Binaries/Dafny /noVerify /compileTarget:go /compile:2 /compileVerbose:0 test.dfy > tmp.txt 2>>errors/compErrors/go.txt
-    if [ $? -eq 0 ]
-    then
-      echo "Created GO files"
-      ./test > "outputs/output-go-$y.txt" 2>>errors/compErrors/go.txt
-      rm -rf test || true
-      rm -rf test-go || true
-    else
-      rm -rf test || true
-      rm -rf test-go || true
-      echo "Failed to convert to GO in $t seconds"
-    fi
+      timeout -s SIGKILL $t ./src/main/dafny_compiler/dafny/Binaries/Dafny verify $file > tmp.txt 2>&1
+      code=$?
+      if [[ $code -ne 4 && $code -lt 5 ]]
+      then
+        echo "Verification error found in test $x - incorrect validation of file $file"
+        mkdir "errors/verificationErrors/$x" || true
+        mkdir "errors/verificationErrors/$x/incorrect" || true
+        cp $file "errors/verificationErrors/$x/incorrect/test-$y.dfy"
+        cat tmp.txt > "errors/verificationErrors/$x/incorrect/verificationOutput-$y.txt"
+      fi
+      rm -rf tmp.txt || true
+      y=$(( $y + 1))
+    done
+  fi
 
-    # js
-    cd "$directory"
-    timeout -s SIGKILL $t ./src/main/dafny_compiler/dafny/Binaries/Dafny /noVerify /compileTarget:js /compile:2 /compileVerbose:0 test.dfy > tmp.txt 2>>errors/compErrors/js.txt
-    if [ $? -eq 0 ]
-    then
-      echo "Created JS files"
-      node test.js > "outputs/output-js-$y.txt" 2>>errors/compErrors/js.txt
-      rm -rf test.js || true
-    else
-      rm -rf test.js || true
-      echo "Failed to convert to JS in $t seconds"
-    fi
+  cd "$directory"
+  y=0
+  if [ "$(ls -A tests/)" ];
+  then
+    for file in tests/*.dfy
+    do
+      echo "Attempting to run $file"
+      cp "$file" test.dfy
 
-    # java
-    cd "$directory"
-    timeout -s SIGKILL $t ./src/main/dafny_compiler/dafny/Binaries/Dafny /noVerify /compileTarget:java /compile:2 /compileVerbose:0 test.dfy  > tmp.txt 2>>errors/compErrors/java.txt
-    if [ $? -eq 0 ]
-    then
-      echo "Created Java files"
-      java -jar test.jar > "outputs/output-java-$y.txt" 2>>errors/compErrors/java.txt
-      rm -rf test.jar || true
-      rm -rf test-java || true
-    else
-      rm -rf test.jar || true
-      rm -rf test-java || true
-      echo "Failed to convert to Java in $t seconds"
-    fi
+      # GO
+      cd "$directory"
+      timeout -s SIGKILL $t ./src/main/dafny_compiler/dafny/Binaries/Dafny /noVerify /compileTarget:go /compile:2 /compileVerbose:0 test.dfy > tmp.txt 2>>errors/compErrors/go.txt
+      if [ $? -eq 0 ]
+      then
+        echo "Created GO files"
+        ./test > "outputs/output-go-$y.txt" 2>>errors/compErrors/go.txt
+        rm -rf test || true
+        rm -rf test-go || true
+      else
+        rm -rf test || true
+        rm -rf test-go || true
+        echo "Failed to convert to GO in $t seconds"
+      fi
 
-    # py
-    cd "$directory"
-    timeout -s SIGKILL $t ./src/main/dafny_compiler/dafny/Binaries/Dafny /noVerify /compileTarget:py /compile:2 /compileVerbose:0 test.dfy > tmp.txt  2>>errors/compErrors/py.txt
-    if [ $? -eq 0 ]
-    then
-      echo "Created Python files"
-      python3 test-py/test.py > "outputs/output-py-$y.txt" 2>>errors/compErrors/py.txt
-      rm -rf test-py || true
-    else
-      rm -rf test-py || true
-      echo "Failed to convert to Python in $t seconds"
-    fi
+      # js
+      cd "$directory"
+      timeout -s SIGKILL $t ./src/main/dafny_compiler/dafny/Binaries/Dafny /noVerify /compileTarget:js /compile:2 /compileVerbose:0 test.dfy > tmp.txt 2>>errors/compErrors/js.txt
+      if [ $? -eq 0 ]
+      then
+        echo "Created JS files"
+        node test.js > "outputs/output-js-$y.txt" 2>>errors/compErrors/js.txt
+        rm -rf test.js || true
+      else
+        rm -rf test.js || true
+        echo "Failed to convert to JS in $t seconds"
+      fi
 
-    # cpp
-  #  ./src/main/dafny_compiler/dafny/Binaries/Dafny /noVerify /compileTarget:cpp /spillTargetCode:3 test.dfy > tmp.txt 2>&1
-  #  echo "Created C++ files"
-  #  g++ test.cpp test.h DafnyRuntime.h  > tmp.txt 2>&1
-  #  ./a.out > outputs/output-cpp.txt
-    rm -rf test.dfy || true
-    rm -rf tmp.txt || true
-    y=$(( $y + 1))
-  done
+      # java
+      cd "$directory"
+      timeout -s SIGKILL $t ./src/main/dafny_compiler/dafny/Binaries/Dafny /noVerify /compileTarget:java /compile:2 /compileVerbose:0 test.dfy  > tmp.txt 2>>errors/compErrors/java.txt
+      if [ $? -eq 0 ]
+      then
+        echo "Created Java files"
+        java -jar test.jar > "outputs/output-java-$y.txt" 2>>errors/compErrors/java.txt
+        rm -rf test.jar || true
+        rm -rf test-java || true
+      else
+        rm -rf test.jar || true
+        rm -rf test-java || true
+        echo "Failed to convert to Java in $t seconds"
+      fi
+
+      # py
+      cd "$directory"
+      timeout -s SIGKILL $t ./src/main/dafny_compiler/dafny/Binaries/Dafny /noVerify /compileTarget:py /compile:2 /compileVerbose:0 test.dfy > tmp.txt  2>>errors/compErrors/py.txt
+      if [ $? -eq 0 ]
+      then
+        echo "Created Python files"
+        python3 test-py/test.py > "outputs/output-py-$y.txt" 2>>errors/compErrors/py.txt
+        rm -rf test-py || true
+      else
+        rm -rf test-py || true
+        echo "Failed to convert to Python in $t seconds"
+      fi
+
+      # cpp
+    #  ./src/main/dafny_compiler/dafny/Binaries/Dafny /noVerify /compileTarget:cpp /spillTargetCode:3 test.dfy > tmp.txt 2>&1
+    #  echo "Created C++ files"
+    #  g++ test.cpp test.h DafnyRuntime.h  > tmp.txt 2>&1
+    #  ./a.out > outputs/output-cpp.txt
+      rm -rf test.dfy || true
+      rm -rf tmp.txt || true
+      y=$(( $y + 1))
+    done
+  fi
   java -cp out/ Main.CompareOutputs $x "./outputs"
   if [ $? -eq 1 ]
   then
